4db7b3281a642714d77dce23c8b99a982536090c
Anne-Marie Dufour <annema.dufour@gmail.com>
2023-03-03 08:08:15 +0100

    Install sass and add initial styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import "./styles/styles.scss";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <div className="container">
+          <h1>Your sensors</h1>
+        </div>
       </header>
     </div>
   );
